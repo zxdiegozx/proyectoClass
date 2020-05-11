3eff3d4f7da31de52c3961c5cad8f3a9486b252a
diego bracamonte <diego.bracamonte77@gmail.com>
2020-05-11 16:42:04 -0400

    Cambio de algoritmo en la visualizacion del display

--- Clasesprueba.cpp
+++ Clasesprueba.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <ctime>
 
 using std::cout; // el programa usa cout
 using std::cin; // el programa usa cin
@@ -7,28 +6,29 @@ using std::endl; // el programa usa endl
 class CalculoNumeros
 {
 public:
-
     void procesoDisplay(int valor)
-    {unsigned t0, t1;
-    t0=clock();
+    {
     num=valor;
     if(valor<0)
     {   negativo=valor;
         valor=-(valor);
     }
 
+     //Calcular cantidad digitos
     if(num<0){num=-(num);}
     while(num/10>0)
     {
         num=num/10;
         contador++;
     }
-           numDes[0]= valor%10;
-           for(int x=1; x<7; x++)
-        {
-            pos*=10;
-            numDes[x]=(valor/pos)%10;
-        }
+
+    //Descopone la lectura
+    numDes[0]= valor%10;
+    for(int x=1; x<7; x++)
+    {
+        pos*=10;
+        numDes[x]=(valor/pos)%10;
+    }
 
     if(negativo>0)
      { cout << "El numero a descomponer es: " << valor <<endl;
@@ -37,73 +37,13 @@ public:
      cout << "El numero tiene estos digitos: " << contador << endl;
       cout << endl;
 
-    switch(contador)
-    {
-        case 1:
-                    if(negativo<0){cout << "-";}
-                    cout << "  " << numDes[0];
-                    cout << endl;
-                     break;
-        case 2:
-                    if(negativo<0){cout << "-";}
-                    cout << "  " << numDes[1];
-                    cout << "  " << numDes[0];
-                    cout << endl;
-                     break;
-
-        case 3:
-                    if(negativo<0){cout << "-";}
-                   cout << "  " << numDes[2];
-                   cout << "  " << numDes[1];
-                   cout << "  " << numDes[0];
-                    cout << endl;
-                     break;
-
-        case 4:
-                    if(negativo<0){cout << "-";}
-                   cout << "  " << numDes[3];
-                   cout << "  " << numDes[2];
-                   cout << "  " << numDes[1];
-                   cout << "  " << numDes[0];
-                    cout << endl;
-                     break;
-
-        case 5:
-                    if(negativo<0){cout << "-";}
-                    cout << "  " << numDes[4];
-                    cout << "  " << numDes[3];
-                    cout << "  " << numDes[2];
-                    cout << "  " << numDes[1];
-                    cout << "  " << numDes[0];
-                    cout << endl;
-                     break;
-
-        case 6:
-                    if(negativo<0){cout << "-";}
-                   cout << "  " << numDes[5];
-                   cout << "  " << numDes[4];
-                   cout << "  " << numDes[3];
-                   cout << "  " << numDes[2];
-                   cout << "  " << numDes[1];
-                   cout << "  " << numDes[0];
-                    cout << endl;
-                     break;
-
-        case 7:
-                    if(negativo<0){cout << "-";}
-                    for(xi=6;xi==0;xi--)
-                    {
-                        cout << "  " << numDes[xi];
-                        cout << endl;
-                    }break;
-        default:
-            break;
-
-    }          t1 = clock();
-long double time = (double(t1-t0)/CLOCKS_PER_SEC);
-cout << "Execution Time code: " << time << endl;
+      //visualiza numero descompuesto
+      if(negativo<0){cout << "-"<<endl;}
+        for(int xi=0; xi<contador; xi++)
+        {
+            cout<<xi<<": "<<numDes[xi]<<endl;
+        }
     }
-
     private:
     int num=1,contador=1, pos=1, xi;
     int numDes[7];
@@ -119,3 +59,4 @@ int main()
     miCalculoNumeros.procesoDisplay(Numero);
     return 0;
 }
+
